ae0ea7594c65bab3b3eb058d578f3f7ddd0e3a90
mb <nerooren4001@gmail.com>
2023-11-30 01:39:19 +0300

    Add messages for success operations

--- candidates/App/views.py
+++ candidates/App/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, ListView, DeleteView
-
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from .models import Candidate
 
 
-class Create(CreateView):
+class Create(SuccessMessageMixin, CreateView):
     model = Candidate
     fields = '__all__'
     success_url = reverse_lazy('read')
+    success_message = "Candidate : %(name)s created succesfully!"
 
 
 class Read(ListView):
@@ -16,18 +18,16 @@ class Read(ListView):
     queryset = Candidate.objects.all()
 
 
-class Update(UpdateView):
+class Update(SuccessMessageMixin, UpdateView):
     model = Candidate
     fields = '__all__'
     success_url = reverse_lazy('read')
+    success_message = "Candidate : %(name)s updated succesfully!"
 
 
 class Delete(DeleteView):
-    queryset = Candidate.objects.all()
-    success_url = reverse_lazy('read')
+    model = Candidate
 
-
-def handler500(request):
-    response = render(request, '500.html', {})
-    response.status_code = 500
-    return response
+    def get_success_url(self):
+        messages.success(self.request, "Candidate deleted succesfully! ")
+        return reverse('read')
